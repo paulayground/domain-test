75ce1d565f36d6f8e0e8fb08470041a6c35b98c9
paul <paul@stevelabs.co>
2023-05-28 17:12:06 +0900

    sdc

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
-        secure: false,
+        secure: true,
         domain: undefined,
         sameSite: "none",
       },
